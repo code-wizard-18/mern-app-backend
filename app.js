378c12b3f410ca81abaf8b5df437aed44b9cb3c9
@nithishkumar18 <nithish@thetopclassentertainment.com>
2024-10-23 18:15:29 +0530

    added the changes

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
-console.log("MONGO_URI :",process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
